1aa3c9a2d1ecb6e3d0bd35b9eae29086c39ab656
Ramiro quispe <ramiroquisper@gmail.com>
2021-10-21 05:44:03 -0500

    Improve VehicleType

---
+++ Vehicles09.API/Controllers/VehicleTypesController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Vehicles09.API.Data;
+using Vehicles09.API.Data.Entities;
+
+namespace Vehicles09.API.Controllers
+{
+    public class VehicleTypesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public VehicleTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.VehicleTypes.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(VehicleType vehicleType)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(vehicleType);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este tipo de vehiculo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(vehicleType);
+        }
+
+        // GET: VehicleTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleType);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este tipo de vehiculo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+
+            }
+            return View(vehicleType);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            _context.VehicleTypes.Remove(vehicleType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Vehicles09.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vehicles09.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
